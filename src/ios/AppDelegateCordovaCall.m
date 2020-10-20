0fd317db8a37b64510aed0600352eb233c25fd22
toriqo <1195165+toriqo@users.noreply.github.com>
2020-10-20 18:17:50 +0300

    swizzle the app delegate to avoid conflicts with other plugins

--- src/ios/AppDelegateCordovaCall.m
+++ src/ios/AppDelegateCordovaCall.m
@@ -5,12 +5,43 @@
 
 @implementation AppDelegate (CordovaCall)
 
-- (BOOL)application:(UIApplication *)application continueUserActivity:(NSUserActivity *)userActivity restorationHandler:(void (^)(NSArray *restorableObjects))restorationHandler
++ (void)load {
+    static dispatch_once_t onceToken;
+    
+    dispatch_once(&onceToken, ^{
+        [self swizzleMethod:@selector(application:openURL:options:)];
+        [self swizzleMethod:@selector(application:continueUserActivity:restorationHandler:)];
+    });
+}
+
++ (void)swizzleMethod:(SEL)originalSelector {
+    Class class = [self class];
+    NSString *selectorString = NSStringFromSelector(originalSelector);
+    SEL newSelector = NSSelectorFromString([@"swizzled_" stringByAppendingString:selectorString]);
+    SEL defaultSelector = NSSelectorFromString([@"default_" stringByAppendingString:selectorString]);
+    Method originalMethod = class_getInstanceMethod(class, originalSelector);
+    Method newMethod = class_getInstanceMethod(class, newSelector);
+    Method noopMethod = class_getInstanceMethod(class, defaultSelector);
+    if (class_addMethod(class, originalSelector, method_getImplementation(newMethod), method_getTypeEncoding(newMethod))) {
+        class_replaceMethod(class, newSelector, method_getImplementation(originalMethod ?: noopMethod), method_getTypeEncoding(originalMethod));
+    } else {
+        method_exchangeImplementations(originalMethod, newMethod);
+    }
+}
+
+- (BOOL)default_application:(UIApplication *)app continueUserActivity:(NSUserActivity *)userActivity restorationHandler:(void (^)(NSArray *))restorationHandler {
+    return FALSE;
+}
+
+- (BOOL)swizzled_application:(UIApplication *)application continueUserActivity:(NSUserActivity *)userActivity restorationHandler:(void (^)(NSArray *restorableObjects))restorationHandler
 {
     INInteraction *interaction = userActivity.interaction;
     INIntent *intent = interaction.intent;
     BOOL isVideo = [intent isKindOfClass:[INStartVideoCallIntent class]];
     INPerson *contact;
+    NSLog(@"%@", contact);
+    NSLog(isVideo ? @"YES" : @"NO");
+    NSLog(@"%@", intent);
     if(isVideo) {
         INStartVideoCallIntent *startCallIntent = (INStartVideoCallIntent *)intent;
         contact = startCallIntent.contacts.firstObject;
@@ -26,6 +57,7 @@
     }
     NSDictionary *intentInfo = @{ @"callName" : callName, @"callId" : callId, @"isVideo" : isVideo?@YES:@NO};
     [[NSNotificationCenter defaultCenter] postNotificationName:@"RecentsCallNotification" object:intentInfo];
+    
     return YES;
 }
 @end
